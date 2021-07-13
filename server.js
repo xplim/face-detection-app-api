8c5690f49a274b181b50d4d95e090cc40564c6c5
xplim <75422036+xplim@users.noreply.github.com>
2021-07-13 17:45:40 +0800

    Set up database

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 'use strict';
 
 import cors from 'cors';
+import dotenv from 'dotenv';
 import express from 'express';
+import knex from 'knex';
 
 import router from './router';
 
 const PORT = 4000;
 
 const app = express();
+
+dotenv.config();
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+});
 
 app.use(cors());
 app.use(express.json());
